02ad28a06fcd4b933cd5876d26a61bb7996d6ced
Austin-J-B <156863811+Austin-J-B@users.noreply.github.com>
2025-08-16 16:41:31 -0500

    small update

--- tomcat/handlers/dues.py
+++ tomcat/handlers/dues.py
@@ -1,4 +1,4 @@
 from __future__ import annotations
 import discord
 async def handle_dues_notice(intent, ctx):
-    await ctx["channel"].send("Dues notice recorded. (Gmail/ledger integration to be added)")
+    await ctx["channel"] #.send("Dues notice recorded. (Gmail/ledger integration to be added)")
